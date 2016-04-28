8d4437d13bc07899a1e052008a520e6ff0f825a0
jonibegoode <drjekillmrconnard@hotmail.com>
2016-04-28 16:59:26 +0200

    lol

--- lorem1.php
+++ lorem1.php
@@ -4,10 +4,10 @@ require_once 'inc/config.php';
 $contents = nl2br(file_get_contents('lorem.txt'));
 $contents = explode('<br />'.PHP_EOL.'<br />'.PHP_EOL, $contents);
 
-print_r($contents);
-/*foreach ($contents as $key => $content) {
-	fopen();
-}*/
+debug($contents);
+foreach ($contents as $key => $content) {
+	file_put_contents('lorem/lorem'.$key.'.txt', $content);
+}
 
 
  ?>
